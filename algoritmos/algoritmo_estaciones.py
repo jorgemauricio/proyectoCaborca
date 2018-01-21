0f4afba842c8b832c71df7fbd3a390b2e645e46a
jorgemauricio <jorge.ernesto.mauricio@gmail.com>
2018-01-20 19:21:53 -0600

    generar mapas interpolados de las estaciones por PM y Tiempo

--- algoritmos/algoritmo_estaciones.py
+++ algoritmos/algoritmo_estaciones.py
@@ -18,11 +18,15 @@ from scipy.interpolate import griddata as gd
 import os
 import numpy as np
 import pandas as pd
+from random import *
 
 # programa principal
 def main():
 	# constantes
 	RUTA_CARPETA = "/home/jorge/Documents/Research/proyectoCaborca"
+
+	# colores
+	temasColores = ["Greens", "PuBu", "coolwarm", "viridis", "inferno"]
 	
 	# fecha actual
 	fechaActual = strftime("%Y-%m-%d")
@@ -56,7 +60,8 @@ def main():
 		rutaDatosEstaciones = "{}/estaciones/{}-{:02d}-{:02d}/{}.csv".format(RUTA_CARPETA, anio, mes, dia,i)
 		print(rutaDatosEstaciones)
 		dataTemp = pd.read_csv(rutaDatosEstaciones)
-		print(dataTemp.head())
+		
+		dataTemp["ID"] = i
 		
 		# valor de longitud
 		# latTemp = dataEstaciones.loc[dataEstaciones["ID"]==i]["Lat"]
@@ -69,12 +74,16 @@ def main():
 		lonTemp = dataEstaciones.iloc[i-1]["Long"]
 		dataTemp["Lon"] = lonTemp
 		print("***** Lon", lonTemp)
+		dataTemp.fillna(value=0.0)
 		frames.append(dataTemp)
 		print(dataTemp.head())
 
 	# merge frames
 	data =  pd.concat(frames)
 
+	# save info
+	rutaCarpetaParaGuardarCSV = "{}/estaciones/{}-{:02d}-{:02d}/{}-{:02d}-{:02d}.csv".format(RUTA_CARPETA, anio, mes, dia,anio, mes, dia)
+	data.to_csv(rutaCarpetaParaGuardarCSV)
 	# array columnas a evaluar
 	arrayColumns = ["PM1_1M", "PM1_5M","PM1_15M","PM2P5_1M","PM2P5_5M","PM2P5_15M","PM2P5_1H","PM1_1H","PM10_1M","PM10_5M","PM10_15M","PM10_1H"]
 
@@ -101,11 +110,11 @@ def main():
 			print(y_values)
 
 			# limites min y max
-			LONG_MIN = x_values[0] - 0.1
-			LONG_MAX = x_values[-1] + 0.1
+			LONG_MIN = x_values[0] - 0.2
+			LONG_MAX = x_values[-1] + 0.2
 
-			LAT_MIN = y_values[0] - 0.1
-			LAT_MAX = y_values[-1] + 0.1
+			LAT_MIN = y_values[0] - 0.2
+			LAT_MAX = y_values[-1] + 0.2
 
 			# limites longitud > -115.65 y < -107.94
 			dataProcesamiento = dataProcesamiento.loc[dataProcesamiento['Lon'] > LONG_MIN]
@@ -119,9 +128,10 @@ def main():
 			lons = np.array(dataProcesamiento['Lon'])
 			lats = np.array(dataProcesamiento['Lat'])
 
+			print("*****\nDATOS PROCESAMIENTO\n*****",dataProcesamiento.head())
 			
 			# iniciar la gráfica
-			#plt.clf()
+			plt.clf()
 
 			# agregar locación de estaciones
 			xC = np.array(dataEstaciones['Long'])
@@ -130,56 +140,66 @@ def main():
 			m = Basemap(projection='mill',llcrnrlat=LAT_MIN,urcrnrlat=LAT_MAX,llcrnrlon=LONG_MIN,urcrnrlon=LONG_MAX,resolution='h')
 
 			# generar lats, lons
-			#x, y = m(lons, lats)
+			x, y = m(lons, lats)
 
 			# numero de columnas y filas
-			#numCols = len(x)
-			#numRows = len(y)
+			numCols = len(x)
+			numRows = len(y)
 
+			print("cols, rows", numCols, numRows)
 			# generar xi, yi
-			#xi = np.linspace(x.min(), x.max(), numCols)
-			#yi = np.linspace(y.min(), y.max(), numRows)
+			xi = np.linspace(x.min()-1, x.max()+1, 100)
+			yi = np.linspace(y.min()-1, y.max()+1, 100)
 
 			# generar el meshgrid
-			#xi, yi = np.meshgrid(xi, yi)
+			xi, yi = np.meshgrid(xi, yi)
 
 			# generar zi
 			z = np.array(dataProcesamiento[columna])
-			#zi = gd((x,y), z, (xi,yi), method='cubic')
+			zi = gd((x,y), z, (xi,yi), method='cubic')
 
 			# generar clevs
-			#stepVariable = 1
-			#step = (z.max() - z.min()) / 10
-
-			# verificar el valor del intervalo
-			#if step <= 1:
-			#    stepVariable = 1
+			stepVariable = 1
+			
+			def generarClevs(z):
+				step = (z.max() - z.min()) / 10
+				print("***** MAX Z", z.max())
+				print("***** MIN Z", z.min())
+				if step <= 0.1:
+					return np.linspace(0.0,1,10)
+				else:
+					return np.linspace(0, z.max(), 10)
+			
+			clevs = generarClevs(z)
 
 			#clevs = np.linspace(z.min(), z.max() + stepVariable , 10)
+			
 			#clevs = [1,2,3,4,5,6,7,8,9,10]
 
 			# contour plot
-			#cs = m.contourf(xi,yi,zi, clevs, zorder=5, alpha=0.5, cmap='PuBu')
+
+			cs = m.contourf(xi,yi,zi, clevs, zorder=5, alpha=0.5, cmap="hot_r")
 			
 			# agregar archivo shape de estados
 			m.readshapefile('../shapes/Estados', 'Estados')
 
 			# agregar puntos de estaciones
-			m.scatter(xC, yC, latlon=True,s=z, cmap='coolwarm', marker='o', zorder=25)
+			m.scatter(xC, yC, latlon=True,s=1, color="r", marker='o', zorder=25)
 
 			# colorbar
-			#cbar = m.colorbar(cs, location='right', pad="5%")
-			#cbar.set_label('mm')
+			cbar = m.colorbar(cs, location='right', pad="5%")
+			cbar.set_label('mm')
 			tituloTemporalParaElMapa = "{} {}-{:02d}-{:02d}".format(columna,anio,mes,dia)
-			(tituloTemporalParaElMapa)
 
 			# Mac /Users/jorgemauricio/Documents/Research/proyectoGranizo/Maps/{}_{}.png
 			# Linux /home/jorge/Documents/Research/proyectoGranizo/Maps/{}_{}.png
-			nombreTemporalParaElMapa = "{}/estaciones/maps/{}-{:02d}-{:02d}/{}.png".format(RUTA_CARPETA, anio, mes, dia, columna)
+			print("***** PROCESAMIENTO PARA: ", columna)
+			nombreTemporalParaElMapa = "{}/estaciones/maps/{}-{:02d}-{:02d}/{}_{}.png".format(RUTA_CARPETA, anio, mes, dia, t,columna)
 			plt.annotate('@2018 INIFAP', xy=(-109,29), xycoords='figure fraction', xytext=(0.45,0.45), color='g', zorder=50)
 
 			plt.savefig(nombreTemporalParaElMapa, dpi=300)
 			print('****** Genereate: {}'.format(nombreTemporalParaElMapa))
+
 		
 
 def generarDiaAnterior(f):
